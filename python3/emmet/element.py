4a4bbec8d45e36c7e4fc0531aac531352f5f5cd5
Adriaan Zonnenberg <amz@adriaan.xyz>
2019-01-11 15:13:31 +0100

    Add initial ElementCollection and implement a count on node objects

--- python3/emmet/element.py
+++ python3/emmet/element.py
@@ -1,15 +1,58 @@
 from __future__ import annotations
 from typing import List, Optional, Union
+from copy import copy
 
 
 class ElementCollection:
-    pass
+    items: List[Union[Element, Text]]
+
+    def __init__(self, items=None):
+        if isinstance(items, self.__class__):
+            return items
+        elif items:
+            self.items = items
+        else:
+            items = []
+
+    def __len__(self):
+        length = 0
+        for i in self.items:
+            length += i.count if isinstance(i, Element) else 1
+        return length
+        # return reduce(lambda length, i: length + i.count, self.items)
+
+    def __iter__(self):
+        return self.flatten()
+
+    def flatten(self):
+        items = []
+        for i in self.items:
+            for nth in range(i.count):
+                # maybe move this for loop to a to_list() function on Element
+                repeated_item = copy
+                repeated_item.count = 1
+                items += [repeated_item]
+        return items
+
+
+class Text:
+    body: str
+    count = 1
+    # an "nth" property may be needed when supporting the ($) operator
+
+    def __init__(self, body=""):
+        self.body = body
+
+    def __str__(self):
+        return self.body
 
 
 class Element:
     name: str
     attributes: List[Attribute]
     content: List[Union[Element, str]]
+    count = 1
+    # an "nth" property may be needed when supporting the ($) operator
 
     def __init__(self, name="div"):
         self.name = name
--- python3/emmet/element_test.py
+++ python3/emmet/element_test.py
@@ -1,5 +1,5 @@
 import unittest
-from emmet.element import Attribute, Element
+from emmet.element import Attribute, Element, ElementCollection
 
 
 class TestElementStringRepresentations(unittest.TestCase):
@@ -50,6 +50,30 @@ class TestElementStringRepresentations(unittest.TestCase):
         self.assertEqual(
             "<div>\n\t<p>{}</p>\n\t<p>{}</p>\n\t<p>{}</p>\n</div>", str(el)
         )
+
+
+class TestElementCollection(unittest.TestCase):
+    def test_length(self):
+        self.assertEqual(2, len(ElementCollection([Element(), Element()])))
+
+    def test_element_count_is_added_to_length(self):
+        el = Element()
+        el.count = 3
+        self.assertEqual(4, len(ElementCollection([Element(), el])))
+
+    def test_flatten(self):
+        el = Element()
+        el.count = 3
+        flattened = ElementCollection([el]).flatten()
+        self.assertIsInstance(flattened, list)
+        self.assertEqual(3, len(flattened))
+
+    def test_flatten_resets_count_of_children(self):
+        el = Element()
+        el.count = 3
+        flattened = ElementCollection([el]).flatten()
+        for item in flattened:
+            self.assertEqual(1, item.count)
 
 
 if __name__ == "__main__":
