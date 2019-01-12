17e6b69c716fcd71850f898fe4244eba5f4a0b1f
Adriaan Zonnenberg <amz@adriaan.xyz>
2019-01-12 14:11:22 +0100

    Don't create reference to the "current" element while parsing
    Instead, always assume the last element is the one that should be
    mutated.
    
    This is in preparation for parsing nested elements (elements other than
    the only one being parsed in expand_abbreviation).

--- python3/emmet/__init__.py
+++ python3/emmet/__init__.py
@@ -5,30 +5,31 @@ from emmet.parser import Parser
 
 
 def expand_abbreviation(input: str) -> str:
-    elements = ElementCollection()
-    el = Element()
-    elements.append(el)
+    e = ElementCollection(Element())
     parser = Parser(input)
 
-    parser.extract_element_name(lambda name: el.set_name(name))
+    parser.extract_element_name(lambda name: e[-1].set_name(name))
 
     while parser.input:
-        if parser.extract_class(lambda name: el.add_to_attribute("class", name)):
+        if parser.extract_class(lambda name: e[-1].add_to_attribute("class", name)):
             continue
 
-        if parser.extract_id(lambda name: el.set_attribute("id", [name])):
+        if parser.extract_id(lambda name: e[-1].set_attribute("id", [name])):
             continue
 
         # extract custom attributes...
         # extract content...
 
-        if parser.extract_repeat(lambda times: el.set_repeat(times)):
+        if parser.extract_repeat(lambda times: e[-1].set_repeat(times)):
             continue
+
+        # if parser.extract_nest(lambda name: e.append(Element(name))):
+        #     pass
 
         # stop parsing when unrecognized content was found
         break
 
-    return add_tabstops(str(elements))
+    return add_tabstops(str(e))
 
 
 def add_tabstops(html: str):
